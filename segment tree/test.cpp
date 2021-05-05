4b0e9c726a78b92a45f7cca857aa1057a72dec32
shadabeqbal <shadabeqbal2008@gmail.com>
2021-05-05 18:14:41 +0530

    commit

---
+++ segment tree/test.cpp
@@ -0,0 +1,99 @@
+/*  // //  
+   // //        
+   \\//       
+	\>>      
+	//\\    
+   //  \\
+Author: Shadab Eqbal 
+Created on: "18-04-2021" 
+Name: Create Segment Tree
+Link: 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 1e18
+#define MIN -1e18
+#define MOD 1000000007
+#define PI 3.141592653589793238
+
+#define pb push_back
+#define mp make_pair
+#define ll long long int
+
+#define all(x) x.begin(), x.end()
+
+#define clr(x) memset(&x, 0, sizeof(x))
+
+#define srt(x) sort(all(x))
+#define search(x, y) binary_search(all(x), y)
+
+#define fr(x, n) for (ll i = x; i < n; i++)
+#define frk(x, n, k) for (ll i = x; i < n; i += k)
+#define frit(x) for (auto i = x.begin(); i != x.end(); ++i)
+
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef vector<vpi> vvpi;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpl;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+struct node
+{
+    int left_idx;
+    int right_idx;
+    int sum;
+    struct node *left;
+    struct node *right;
+};
+struct node *foo(struct node *root, vi &vec, int low, int high)
+{
+    if (low == high)
+    {
+        node *p = new node();
+        p->left_idx = low;
+        p->right_idx = high;
+        p->sum = vec[low];
+        p->left=NULL;
+        p->right=NULL;
+        cout<<p->sum;
+        return p;
+    }
+    node *x=new node();
+    x->left_idx=low;
+    x->right_idx=high;
+    int mid = low + (high - low) / 2;
+    
+    x->left = foo(x->left, vec, low, mid);
+    x->right = foo(x->right, vec, mid + 1, high);
+    x->sum = (x->left->sum) + (x->right->sum);
+    return x;
+}
+void display(struct node *p)
+{
+    if (p->left_idx == p->right_idx)
+    {
+        return;
+    }
+    display(p->left);
+    cout << p->sum<<"\n";
+    display(p->right);
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    vi vec = {1, 5, 8, 7, 6, 3, 2};
+    node *root = new node();
+    int high = vec.size() - 1;
+    int low = 0;
+    root=foo(root, vec, low, high);
+    display(root);
+    return 0;
+}
---
+++ tries/prefix_and_suffix.cpp
@@ -0,0 +1,127 @@
+/*  // //  
+   // //        
+   \\//       
+    \>>      
+    //\\    
+   //  \\
+Author: Shadab Eqbal 
+Created on: "04-05-2021" 
+Name: Shortest Unique Prefix
+Link: 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 1e18
+#define MIN -1e18
+#define MOD 1000000007
+#define PI 3.141592653589793238
+
+#define pb push_back
+#define mp make_pair
+#define ll long long int
+
+#define all(x) x.begin(), x.end()
+
+#define clr(x) memset(&x, 0, sizeof(x))
+
+#define srt(x) sort(all(x))
+#define search(x, y) binary_search(all(x), y)
+
+#define fr(x, n) for (ll i = x; i < n; i++)
+#define frk(x, n, k) for (ll i = x; i < n; i += k)
+#define frit(x) for (auto i = x.begin(); i != x.end(); ++i)
+
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef vector<vpi> vvpi;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpl;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+struct TrieNode
+{
+    char ch;
+    map<char, TrieNode *> m;
+    int score;
+};
+
+TrieNode *createTrie(vector<string> &A)
+{
+
+    TrieNode *root = new TrieNode();
+    TrieNode *temp = new TrieNode();
+    root->ch = '$';
+    root->score = 0;
+    temp = root;
+    for (int i = 0; i < (int)A.size(); i++)
+    {
+        for (int j = 0; j < (int)A[i].size(); j++)
+        {
+            if (temp->m[A[i][j]] == 0)
+            {
+                TrieNode *newNode = new TrieNode();
+                newNode->ch = A[i][j];
+                newNode->score = 1;
+                temp->m[A[i][j]] = newNode;
+                temp = temp->m[A[i][j]];
+            }
+            else
+            {
+                temp = temp->m[A[i][j]];
+                temp->score++;
+            }
+        }
+        temp = root;
+    }
+
+    return root;
+}
+
+vector<string> traversal(TrieNode *root, vector<string> &A)
+{
+    TrieNode *temp = new TrieNode();
+    temp = root;
+    vector<string> ans;
+    string str = "";
+    for (int i = 0; i < (int)A.size(); i++)
+    {
+        temp = temp->m[A[i][0]];
+        str += A[i][0];
+        for (int j = 1; j <= (int)A[i].size(); j++)
+        {
+            if (temp->score == 1)
+            {
+                ans.push_back(str);
+                str = "";
+                break;
+            }
+            else
+            {
+                str += A[i][j];
+                temp = temp->m[A[i][j]];
+            }
+        }
+        temp = root;
+    }
+
+    return ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    vector<string> A = {"ababa", "aabbvaab", "aecdsaaec", "abaaxbqaba"};
+    vector<string> B = {"aba", "aec", "abb", "aab"};
+    TrieNode *root = createTrie(A);
+    vector<string> ans = traversal(root, B);
+
+    for (auto i : ans)
+        cout << i << "\n";
+    return 0;
+}
