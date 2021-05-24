50d0084c12e1a3896201b7273eef25fa5cf0710f
shadabeqbal <shadabeqbal2008@gmail.com>
2021-05-24 15:51:47 +0530

    commit

---
+++ extraproblems/check_ith_bit.cpp
@@ -0,0 +1,59 @@
+/*  // //  
+   // //        
+   \\//       
+    \>>      
+    //\\    
+   //  \\
+Author: Shadab Eqbal 
+Created on: "24-05-2021" 
+Name: 
+Link: 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 1e18
+#define MIN -1e18
+#define MOD 1000000007
+#define PI 3.141592653589793238
+
+#define pb push_back
+#define mp make_pair
+#define ll long long int
+
+#define all(x) x.begin(), x.end()
+
+#define clr(x) memset(&x, 0, sizeof(x))
+
+#define srt(x) sort(all(x))
+#define search(x, y) binary_search(all(x), y)
+
+#define fr(x, n) for (ll i = x; i < n; i++)
+#define frk(x, n, k) for (ll i = x; i < n; i += k)
+#define frit(x) for (auto i = x.begin(); i != x.end(); ++i)
+
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef vector<vpi> vvpi;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpl;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n = 102;
+    int k = 6;
+    if ((n & (1 << (k - 1))))
+        cout << "set\n";
+    else
+        cout << "unset\n";
+
+    return 0;
+}
